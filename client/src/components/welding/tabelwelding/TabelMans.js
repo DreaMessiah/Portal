833fcce81c601aa9f88513286aee167a8a563430
Vladimir Barakhtyanskiy <barahtasurgut@gmail.com>
2024-01-11 15:53:22 +0500

    tabel viewform comlited

---
+++ client/src/components/welding/tabelwelding/TabelMans.js
@@ -0,0 +1,23 @@
+import React, {useEffect, useRef} from "react";
+
+
+
+export const TabelMans = () => {
+
+    return (
+        <div className="tabwelding_tabel_tabelman">
+            <div className="tabwelding_tabel_tabelman_upper">
+                <div className="tabwelding_tabel_tabelman_upper_num">№</div>
+                <div className="tabwelding_tabel_tabelman_upper_fio"></div>
+                <div className="tabwelding_tabel_tabelman_upper_title">Бр.Изомова С.Ш.</div>
+                <div className="tabwelding_tabel_tabelman_upper_itogy">Итого за месяц</div>
+            </div>
+            <div className="tabwelding_tabel_tabelman_strock">
+                <div className="tabwelding_tabel_tabelman_strock_num">1</div>
+                <div className="tabwelding_tabel_tabelman_strock_fio">Барахтянский Владимир Алексеевич</div>
+                <div className="tabwelding_tabel_tabelman_strock_calendar"></div>
+                <div className="tabwelding_tabel_tabelman_strock_itogy"></div>
+            </div>
+        </div>
+    )
+}
