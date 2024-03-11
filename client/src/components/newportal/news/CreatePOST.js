a8b8fa35518cc70af22ed552a2c08969803ab7a0
DreaMessiah <ivalan@bk.ru>
2024-03-11 16:11:29 +0500

    Start LoadImage

--- client/src/components/newportal/news/CreatePOST.js
+++ client/src/components/newportal/news/CreatePOST.js
@@ -1,13 +1,16 @@
 import "./style.scss"
 import {Link} from "react-router-dom";
 import {useEffect} from "react";
+import FilesService from "../../../services/FilesService";
+
 
 export const CreatePOST = () => {
-
-
     const thisPost = [];
 
-
+    const loadImage = async () => {
+        const response = FilesService.loadImage()
+        console.log(response.data)
+    }
 
     return (
         <div className="create_new_post">
@@ -19,9 +22,11 @@ export const CreatePOST = () => {
             </div>
             <div className="create_new_post_worklist">
                 <input className="create_new_post_worklist_header" placeholder="Введите заголовок новой публикации" />
-                <div className="create_new_post_worklist_mainimg">
-                    <i class="fa-solid fa-upload"></i>
+
+                <div onClick={(e) => loadImage()} className="create_new_post_worklist_mainimg">
+                    <i className="fa-solid fa-upload"></i>
                 </div>
+
                 <div className="create_new_post_worklist_content">
                     <div className="plus_btn_create_strock">
                         <div className="plus_btn_create_strock_btn">+ content text</div>
@@ -56,7 +61,7 @@ export const CreatePOST = () => {
                     </div>
                     <div className="create_new_post_worklist_content_imgtext">
                         <div className="create_new_post_worklist_content_imgtext_img">
-                            <i class="fa-solid fa-upload"></i>
+                            <i className="fa-solid fa-upload"></i>
                         </div>
                         <textarea className="create_new_post_worklist_content_imgtext_text" placeholder="Введите текст описания изображения"></textarea>
                     </div>
@@ -68,9 +73,9 @@ export const CreatePOST = () => {
                         <div className="plus_btn_create_strock_btn">+ title</div>
                     </div>
                     <div className="create_new_post_worklist_content_tripleimgs">
-                        <div className="create_new_post_worklist_content_tripleimgs_img"><i class="fa-solid fa-upload"></i></div>
-                        <div className="create_new_post_worklist_content_tripleimgs_img"><i class="fa-solid fa-upload"></i></div>
-                        <div className="create_new_post_worklist_content_tripleimgs_img"><i class="fa-solid fa-upload"></i></div>
+                        <div className="create_new_post_worklist_content_tripleimgs_img"><i className="fa-solid fa-upload"></i></div>
+                        <div className="create_new_post_worklist_content_tripleimgs_img"><i className="fa-solid fa-upload"></i></div>
+                        <div className="create_new_post_worklist_content_tripleimgs_img"><i className="fa-solid fa-upload"></i></div>
                     </div>
                     <div className="plus_btn_create_strock">
                         <div className="plus_btn_create_strock_btn">+ content text</div>
