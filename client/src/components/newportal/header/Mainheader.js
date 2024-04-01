d3b0bddc2a695602053d02671361fe95773f200b
DreaMessiah <ivalan@bk.ru>
2024-04-01 09:09:16 +0500

    Update Mainheader.js

--- client/src/components/newportal/header/Mainheader.js
+++ client/src/components/newportal/header/Mainheader.js
@@ -20,7 +20,7 @@ export const MainHeader = () => {
             {/*    <div className="head_block_search_btn"></div>*/}
             {/*</div>*/}
             <div className="head_block_right">
-                <div className="head_block_callback">Обратная связь</div>
+                {/*<div className="head_block_callback">Обратная связь</div>*/}
                 <Link to="/new_lk" className="head_block_lk">
                     <div style={{backgroundImage:`url(/files/profile/${store.user.avatar})`}} className="head_block_lk_photo"></div>
                     <div className="head_block_lk_name">{store.user.full_name}</div>
