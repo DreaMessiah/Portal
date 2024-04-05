730f2bc49962500719b70b43034c73eb7bfc7508
DreaMessiah <ivalan@bk.ru>
2024-04-05 17:27:31 +0500

    123

--- client/src/pages/structure/StructurePage.js
+++ client/src/pages/structure/StructurePage.js
@@ -14,9 +14,25 @@ export default function StructurePage(){
         }
     },[windowWidth])
 
+    function printImage() {
+        // Создаем новое окно браузера
+        const printWindow = window.open('', '_blank');
+
+        // Получаем URL изображения
+        const imageUrl = '/structure/structure.png'
+
+        // Загружаем изображение в новом окне
+        printWindow.document.write('<img src="' + imageUrl + '" onload="window.print()">');
+
+        // Закрываем окно после печати
+        setTimeout(function () {
+            printWindow.close();
+        }, 1000); // Подождите некоторое время, прежде чем закрыть окно
+    }
     const {structure} = useContext(DataContext)
         return (
         <div className='structure'>
+            <div onClick={() => printImage()} className={`button`}>Печать</div>
             <div className={`title`}>Организационная структура ООО "Сургутское РСУ"</div>
             <div className={'image'}><img src={'/structure/structure.png'} alt={'Структура компании'}/></div>
         </div>
