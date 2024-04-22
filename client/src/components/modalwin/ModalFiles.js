2e4f514661545a3cedd0c122ca15550639bc7f71
Vladimir Barakhtyanskiy <barahtasurgut@gmail.com>
2024-04-22 10:34:28 +0500

    234324234

--- client/src/components/modalwin/ModalFiles.js
+++ client/src/components/modalwin/ModalFiles.js
@@ -2,11 +2,11 @@ import "./modalupload.scss";
 
 
 
-export default function ModalFiles ( {heigth = '30vh', width = '1000px',data, active, setActive}){
+export default function ModalFiles ( {heigth = '30vh',data, active, setActive}){
     return (
         <div className={`modal ${active ? 'modal_active' : ''}`}>
 
-            <div style={{height:heigth,width:width}} className='middle-modal-win-files'>
+            <div style={{height:heigth}} className='middle-modal-win-files'>
                 <div className='middle-modal-win-up'>
                     <span className='left-border'></span>
                     <span className='right-border' onClick={() => setActive(false)} style={{color: '#000'}}><i className="fa-solid fa-xmark"></i></span>
